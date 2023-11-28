cc3b91778a28e994dd6e9bca6ff0fcb18ce1de85
lauri <laurialanen1@gmail.com>
2023-11-28 11:21:59 +0200

    Basic Scraper Ready

---
+++ scraper/scraper.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from time import sleep
+from typing import List
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def wait_element(driver, search_type, search_str):
+    element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((search_type, search_str))
+    )
+    return element
+
+
+def wait_elements(driver, search_type, search_str):
+    elements = WebDriverWait(driver, 5).until(
+        EC.presence_of_all_elements_located((search_type, search_str))
+    )
+    return elements
+
+
+def select_groups(driver):
+    available_groups = wait_elements(driver, By.CLASS_NAME, "search-result-row")
+        
+    for group in available_groups:
+        group_name = wait_element(group, By.CLASS_NAME, "code")
+        reserved_classes = wait_element(group, By.CLASS_NAME, "credits")
+        total_classes = reserved_classes.text.split()[0]
+
+        if (int(total_classes) > 0):
+            button = wait_element(group, By.CLASS_NAME, "btn")
+            button.click()
+
+
+def website_controller(driver, study_sector):
+    try:
+        form = wait_element(driver, By.CLASS_NAME, "form-control")
+        form.send_keys(study_sector)
+
+        select_group_search = wait_element(driver, By.CSS_SELECTOR, ".btn-group-toggle > label:nth-child(2)")
+        select_group_search.click()
+        
+    except TimeoutException:
+        print("Cant locate elements")
+        exit(TimeoutException)
+
+
+def main():
+    url = 'https://lukkarit.vamk.fi/#/schedule'
+
+    options = ChromeOptions()
+    options.accept_insecure_certs = True
+
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    website_controller(driver, "LT2022")
+    select_groups(driver)
+
+if __name__ == '__main__':
+    main()
