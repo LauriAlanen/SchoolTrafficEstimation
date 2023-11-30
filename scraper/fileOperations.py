f66d424ed18c7a67920918cdb7b4dbe1f8ca6327
lauri <laurialanen1@gmail.com>
2023-11-30 13:42:51 +0200

    Scraper now goes through all the classes

--- scraper/fileOperations.py
+++ scraper/fileOperations.py
@@ -4,10 +4,12 @@ import json
 from io import StringIO
 
 def saveSchedule(schedule, path):
+    print(f"Created file at {path}")
+
     df = pd.read_json(StringIO(schedule))
     df.drop(["start_date", "event_id", "code", "visible", "color", "subject", 
              "description", "reserved_for", "location", "realizations"],
-            axis=1, inplace=True),
+            axis=1, inplace=True)
     
     with open(path, "wb") as f:
         df.to_csv(f)
@@ -23,3 +25,15 @@ def getAllClasses(class_file_path):
     with open(class_file_path, "rb") as f:
         classes_json = json.load(f)
         return classes_json
+    
+def getOccurance(df):
+    df.sort_values(by=['end_date'])
+    count = df['end_date'].value_counts()
+    return count
+
+def getNextClass():
+    classes = getAllClasses("schedules/all_classes.json")
+
+    for class_name, class_list in classes.items():
+        for sub_class in class_list:
+            yield sub_class
--- scraper/main.py
+++ scraper/main.py
@@ -4,20 +4,26 @@ import fileOperations
 
 def main():
     url = 'https://lukkarit.vamk.fi/#/schedule'
-    driver = restaurantScraper.create_driver(url)
 
-    restaurantScraper.website_controller(driver, "TT2022-2")
-    
-    if(restaurantScraper.select_groups(driver) != 0):
-        print("Failed to locate any groups quitting...")
-        exit()
-    
-    else:
-        php_session_id = restaurantScraper.get_php_session_id(driver)
-        schedule = calendarRequest.get_calendar_with_cookie(php_session_id).text
+    for sub_class in fileOperations.getNextClass():
+        driver = restaurantScraper.create_driver(url)
 
-        fileOperations.saveSchedule(schedule, "schedules/test.csv")
-        df_schedule = fileOperations.readSchedule("schedules/test.csv")
-        print(df_schedule)        
+        restaurantScraper.website_controller(driver, sub_class)
+        
+        if(restaurantScraper.select_groups(driver) != 0):
+            print("Failed to locate any groups quitting...")
+            exit()
+        
+        else:
+            php_session_id = restaurantScraper.get_php_session_id(driver)
+            schedule = calendarRequest.get_calendar_with_cookie(php_session_id).text
+        
+            fileOperations.saveSchedule(schedule, f"schedules/{sub_class}.csv")
+            df_schedule = fileOperations.readSchedule(f"schedules/{sub_class}.csv")
+            occurances = fileOperations.getOccurance(df_schedule)
+            print(occurances)
+        
+        driver.quit()
+        
 if __name__ == '__main__':
     main()
