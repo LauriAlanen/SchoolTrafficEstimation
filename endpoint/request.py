1c636ced8ed28677dcb1c7907e9389d70bcab6ec
lauri <laurialanen1@gmail.com>
2023-12-11 21:26:28 +0200

    Added cors and run on 0.0.0.0

--- endpoint/request.py
+++ endpoint/request.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sys
 
 sys.path.append("traffic_estimation/")
@@ -8,6 +9,7 @@ import estimate as est
 import fileOperations as fo
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/getTraffic', methods=['GET'])
 def getTraffic():
@@ -25,4 +27,4 @@ def getDates():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
